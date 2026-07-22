6fa91639cd70400e2c76f4144db383a493b73317
arsha2343 <arshadraza7029@gmail.com>
2026-07-22 22:26:42 +0530

    Time: 27 ms (84.96%), Space: 82.8 MB (82.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> hasharr;
-        for(int i = 0; i< nums.size();i++){
-            hasharr[nums[i]]++;
-        }
-        for(auto it: hasharr){
-            if(it.second >=2) return true;
-        }
-        return false;
+       sort(nums.begin(),nums.end());
+       for(int i = 0; i < nums.size()-1;i++){
+        if(nums[i] == nums[i+1]) return true;
+       }
+       return false;
     }
 };
