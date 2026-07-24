535237b2d3a7be411b59f5f5e237823ba87aeec9
arsha2343 <arshadraza7029@gmail.com>
2026-07-24 20:54:42 +0530

    Time: 0 ms (100%), Space: 17.6 MB (20.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,38 @@
 class Solution {
 private: 
-    int lowerbound(vector<int>& nums, int target) {
+    int firstoccurence(vector<int>& nums, int target) {
         int low = 0, high = nums.size()-1;
-        int ans = nums.size();
+        int first = -1;
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] >= target) {
-                ans = mid;
+            if(nums[mid] == target) {
+                first = mid;
                 high = mid  - 1;
             }
-            else low = mid + 1;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        return ans;
+        return first;
     }
-    int upperbound(vector<int>& nums, int target) {
+    int lastoccurence(vector<int>& nums, int target) {
         int low = 0, high = nums.size()-1;
-        int ans = nums.size();
+        int last = -1;
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] > target) {
-                ans = mid;
-                high = mid  - 1;
+            if(nums[mid] == target) {
+                last = mid;
+                low = mid  + 1;
             }
-            else low = mid + 1;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        return ans;
+        return last;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb = lowerbound(nums,target);
-        if(lb == nums.size() || nums[lb] != target) return {-1,-1};
-        return {lb, upperbound(nums,target)-1};
-        
+        int first =  firstoccurence(nums,target);
+        if(first == -1) return {-1,-1};
+        int last = lastoccurence(nums, target);
+        return {first, last};       
     }
 };
